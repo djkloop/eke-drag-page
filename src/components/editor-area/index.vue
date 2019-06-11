0f80df09469e48a76fd9c466a4781d68b38b316f
zhangjianlei <djkloopjs@gmail.com>
2019-06-11 19:07:17 +0800

    增加左侧菜单

--- src/components/editor-area/index.vue
+++ src/components/editor-area/index.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>
+  <div class="appic-editor-wrapper">
     编辑区域
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
-
+  computed: {
+    ...mapState({
+      selectWg: state => state.common.selectWg,
+      pageData: state => state.common.pageData,
+    }),
+  },
 }
 </script>
 
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,46 @@
+<style lang="scss">
+.appic-container-show-area {
+  min-width: 400px;
+  max-width: 720px;
+  border-left: 1px solid #e0e0e0;
+  border-right: 1px solid #e0e0e0;
+
+  .btn-bar {
+    height: 45px;
+    line-height: 45px;
+    font-size: 18px;
+    border-bottom: solid 2px #e4e7ed;
+    text-align: right;
+  }
+
+  .el-main {
+    padding: 0;
+    position: relative;
+  }
+}
+</style>
+
 <template>
   <div class="appic-container-form">
-    <el-container>
+    <el-container style="height:100vh;">
       <Header />
       <el-container>
         <components-list />
-        <el-main>Main</el-main>
+        <el-container class="appic-container-show-area">
+          <el-header class="btn-bar" style="height: 45px;">
+            <el-button type="text" size="medium" icon="el-icon-refresh"  class="mg-r15">重置</el-button>
+            <el-button type="text" size="medium" icon="el-icon-view" class="mg-r15">预览</el-button>
+            <el-button type="text" size="medium" icon="el-icon-document" class="mg-r15">保存</el-button>
+          </el-header>
+          <el-main>
+            <editor-area />
+          </el-main>
+        </el-container>
+        <el-aside>
+          good
+        </el-aside>
       </el-container>
-      <el-footer>Header</el-footer>
+      <el-footer>1</el-footer>
     </el-container>
   </div>
 </template>
@@ -14,26 +48,27 @@
 <script>
 import Header from '@/components/header'
 import ComponentsList from '@/components/components-list'
+import EditorArea from '@/components/editor-area'
 
 export default {
   name: 'home',
   components: {
     Header,
     ComponentsList,
+    EditorArea,
   },
 }
 </script>
 
 <style lang="scss" scoped>
-  .el-header, .el-footer {
-    background-color: #24292e;
-    color: hsla(0,0%,100%,.7);
-    z-index: 32;
-  }
-
   .el-main {
     min-height: calc(100vh - 120px);
     background-color: #E9EEF3;
     color: #333;
+  }
+
+  .el-aside,
+  .el-main {
+    overflow: auto;
   }
 </style>
